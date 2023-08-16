b860b0c0e9115fe2f37a0aa7774ff41d915cf304
ryanvanmanen <ryanpvanmanen@gmail.com>
2023-08-16 15:39:42 -0400

    add organizations tab

--- NY_Query/NY_Query_app/app.R
+++ NY_Query/NY_Query_app/app.R
@@ -1,13 +1,15 @@
-
 library(shiny)
 library(shinyjs)
 library(DT)
 library(rsconnect)
 library(googlesheets4)
 
-gs4_auth("rpvm22@gmail.com")
+gs4_deauth()
+
 PWL_data <- read_sheet("https://docs.google.com/spreadsheets/d/1Z58bVNeNLI1F7NRNCjrDATgzPfK5H03JXNKLTypTzjs/edit#gid=0",
-                       na = c("NA"))
+                       na = "NA", range="Sheet1")
+Org_data <- read_sheet("https://docs.google.com/spreadsheets/d/1Z58bVNeNLI1F7NRNCjrDATgzPfK5H03JXNKLTypTzjs/edit#gid=0",
+                       na = "NA", range="Sheet2",col_names = TRUE)
 
 ui <- fluidPage(
   titlePanel("NYS Waterbodies Query"),
@@ -21,8 +23,14 @@ ui <- fluidPage(
                downloadButton("downloadTable1", "Download selected rows as CSV"),
                DTOutput("table1"),
                style = 'width:100%;'
-             )
-    )))
+             )),
+             tabPanel("Watershed Organizations",
+                      mainPanel(
+                        h5("Watershed Organizations"),
+                        DTOutput("table2"),
+                        style = 'width:100%;'
+                      )),
+    ))
 
 server <- function(input, output) {
   data1 <- reactive({
@@ -31,7 +39,13 @@ server <- function(input, output) {
     df$HMW <- paste0("<a href='",df$HMW,"'", 'target="_blank">',df$HMW,"</a>")
     df$Monitoring_Data <- paste0("<a href='",df$Monitoring_Data,"'", 'target="_blank">',df$Monitoring_Data,"</a>")
     df$USGS <- paste0("<a href='",df$USGS,"'", 'target="_blank">',df$USGS,"</a>")
-    
+    return(df)
+  })
+  
+  data2 <- reactive({
+    df <- Org_data
+    df$Link <- paste0("<a href='",df$Link,"'", 'target="_blank">',df$Link,"</a>")
+    #print("Data2 generated successfully.")  # Debugging line
     return(df)
   })
   
@@ -42,10 +56,16 @@ server <- function(input, output) {
   
   output$table1 <- renderDT({
     datatable(data1(),
-              options = list(searchHighlight = TRUE, searching = TRUE,rownames = FALSE,
+              options = list(searchHighlight = TRUE, searching = TRUE,
                              scrollX =TRUE,rowCallback = JS(
                                "function(row, data, displayNum, displayIndex, dataIndex) {
-         var api = this.api();$(row).attr('data-row-index', dataIndex);}")), 
+         var api = this.api();$(row).attr('data-row-index', dataIndex);}")),rownames = FALSE, 
+              filter="top",escape = FALSE, class = 'cell-border stripe')
+  })
+  output$table2 <- renderDT({
+    datatable(data2(),
+              options = list(searchHighlight = TRUE, searching = TRUE,
+                             scrollX =TRUE),rownames = FALSE, 
               filter="top",escape = FALSE, class = 'cell-border stripe')
   })
   
@@ -60,7 +80,8 @@ server <- function(input, output) {
         selected_data <- data1()[selected_rows, ]
         write.csv(selected_data, file, row.names = FALSE)
       }
-    })
+    }
+    )
 }
 
 shinyApp(ui, server)
