5942ab7b72ae6008f16db5f80b544ded315b9b55
ryanvanmanen <ryanpvanmanen@gmail.com>
2023-09-28 17:44:59 -0400

    removed monitoring tab

--- NY_Query/NY_Query_app/app.R
+++ NY_Query/NY_Query_app/app.R
@@ -9,7 +9,7 @@ library(leaflet.extras)
 gs4_deauth()
 
 PWL_data <- read_sheet("https://docs.google.com/spreadsheets/d/1Z58bVNeNLI1F7NRNCjrDATgzPfK5H03JXNKLTypTzjs/edit#gid=0",
-                       na = "NA", range="Sheet1")
+                       na = "NA", range="Sheet1",col_names = TRUE)
 Org_data <- read_sheet("https://docs.google.com/spreadsheets/d/1Z58bVNeNLI1F7NRNCjrDATgzPfK5H03JXNKLTypTzjs/edit#gid=0",
                        na = "NA", range="Sheet2",col_names = TRUE)
 Lake_location <-read_sheet("https://docs.google.com/spreadsheets/d/1Z58bVNeNLI1F7NRNCjrDATgzPfK5H03JXNKLTypTzjs/edit#gid=0",
@@ -30,20 +30,21 @@ ui <- fluidPage(
                DTOutput("table1"),
                style = 'width:100%;'
              )),
-             tabPanel("Watershed Organizations",
-                      mainPanel(
-                        h5("Watershed Organizations"),
-                        DTOutput("table2"),
-                        style = 'width:100%;'
-                      )),
-    tabPanel("Monitoring Locations",
-             mainPanel(
-               h5("Monitoring Locations"),
-               leafletOutput("map"),
-               style = 'width:100%;', 
-               style ='height:125%;'
-             )),
-    ))
+             # tabPanel("Watershed Organizations",
+             #          mainPanel(
+             #            h5("Watershed Organizations"),
+             #            DTOutput("table2"),
+             #            style = 'width:100%;'
+             #          ))
+  ))
+    ##tabPanel("Monitoring Locations",
+    ##         mainPanel(
+    ##           h5("Monitoring Locations"),
+    ##          leafletOutput("map"),
+    ##           style = 'width:100%;', 
+    ##           style ='height:125%;'
+    ##         )),
+    ##))
 
 server <- function(input, output) {
   data1 <- reactive({
@@ -52,6 +53,7 @@ server <- function(input, output) {
     df$HMW <- paste0("<a href='",df$HMW,"'", 'target="_blank">',df$HMW,"</a>")
     df$Monitoring_Data <- paste0("<a href='",df$Monitoring_Data,"'", 'target="_blank">',df$Monitoring_Data,"</a>")
     df$USGS <- paste0("<a href='",df$USGS,"'", 'target="_blank">',df$USGS,"</a>")
+    #df$CSLAP <- paste0("<a href='",df$CSLAP,"'", 'target="_blank">',df$CSLAP,"</a>")
     return(df)
   })
   
@@ -82,12 +84,12 @@ server <- function(input, output) {
          var api = this.api();$(row).attr('data-row-index', dataIndex);}")),rownames = FALSE, 
               filter="top",escape = FALSE, class = 'cell-border stripe')
   })
-  output$table2 <- renderDT({
-    datatable(data2(),
-              options = list(searchHighlight = TRUE, searching = TRUE,
-                             scrollX =TRUE),rownames = FALSE, 
-              filter="top",escape = FALSE, class = 'cell-border stripe')
-  })
+  # output$table2 <- renderDT({
+  #   datatable(data2(),
+  #             options = list(searchHighlight = TRUE, searching = TRUE,
+  #                            scrollX =TRUE),rownames = FALSE, 
+  #             filter="top",escape = FALSE, class = 'cell-border stripe')
+  # })
   
   output$downloadTable1 <- downloadHandler(
     filename = function() {
@@ -103,41 +105,41 @@ server <- function(input, output) {
     }
     )
 
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      setView(lng = -75.849, lat = 42.7085, zoom = 6.8) %>%
-      addProviderTiles(providers$Esri.WorldTopoMap) %>%
-      addCircleMarkers(data=data3(),
-                       lng=data3()$LONGITUDE, lat=data3()$LATITUDE,
-                       popup = ~paste("Lake Name: ",data3()$LAKE_NAME, "<br>LAKE ID: ",data3()$LAKE_ID,
-                                      "<br>PWL: ", data3()$PWL_ID,"<br>Class: ", data3()$CLASS,
-                                      "<br>County: ", data3()$LAKE_COUNTIES),
-                       radius = 2,
-                       color = "#6B9B6B",
-                       fillOpacity = 0.4, group = "Lake Sites")%>%
+  ##output$map <- renderLeaflet({
+  ##  leaflet() %>%
+  ##    setView(lng = -75.849, lat = 42.7085, zoom = 6.8) %>%
+  ##    addProviderTiles(providers$Esri.WorldTopoMap) %>%
+  ##    addCircleMarkers(data=data3(),
+  ##                     lng=data3()$LONGITUDE, lat=data3()$LATITUDE,
+  ##                     popup = ~paste("Lake Name: ",data3()$LAKE_NAME, "<br>LAKE ID: ",data3()$LAKE_ID,
+  ##                                    "<br>PWL: ", data3()$PWL_ID,"<br>Class: ", data3()$CLASS,
+  ##                                    "<br>County: ", data3()$LAKE_COUNTIES),
+  ##                     radius = 2,
+  ##                     color = "#6B9B6B",
+  ##                     fillOpacity = 0.4, group = "Lake Sites")%>%
     
-      addCircleMarkers(data=data4(),
-                     lng=data4()$LONGITUDE, lat=data4()$LATITUDE,
-                     popup = ~paste("Stream Name: ",data4()$STREAM_NAME, "<br>LAKE ID: ",data4()$STREAM_ID,
-                                  "<br>PWL: ", data4()$PWL_ID,
-                                    "<br>Description: ", data4()$description),
-                     radius = 2,
-                     color = "#FECB00",
-                     fillOpacity = 0.8, group="Stream Sites") %>%
-    addLayersControl(
-      overlayGroups = c("Lake Sites", "Stream Sites"),
-      options = layersControlOptions(collapsed = FALSE)
-    )%>%
+  ##    addCircleMarkers(data=data4(),
+  ##                   lng=data4()$LONGITUDE, lat=data4()$LATITUDE,
+  ##                   popup = ~paste("Stream Name: ",data4()$STREAM_NAME, "<br>LAKE ID: ",data4()$STREAM_ID,
+  ##                                "<br>PWL: ", data4()$PWL_ID,
+  ##                                  "<br>Description: ", data4()$description),
+  ##                   radius = 2,
+  ##                   color = "#FECB00",
+  ##                   fillOpacity = 0.8, group="Stream Sites") %>%
+  ##  addLayersControl(
+  ##    overlayGroups = c("Lake Sites", "Stream Sites"),
+  ##    options = layersControlOptions(collapsed = FALSE)
+  ##  )%>%
       
-      addSearchFeatures(
-        targetGroups = c("Lake Sites", "Stream Sites"), # group should match addMarkers() group
-        options = searchFeaturesOptions(
-          zoom=12, openPopup = TRUE, firstTipSubmit = TRUE,
-          autoCollapse = TRUE, hideMarkerOnCollapse = TRUE
-        )
-      )
+   ##   addSearchFeatures(
+   ##     targetGroups = c("Lake Sites", "Stream Sites"), # group should match addMarkers() group
+   ##     options = searchFeaturesOptions(
+   ##       zoom=12, openPopup = TRUE, firstTipSubmit = TRUE,
+  ##      autoCollapse = TRUE, hideMarkerOnCollapse = TRUE
+    ##    )
+    ##  )
       
-  })
+  ##})
   
 }
 
